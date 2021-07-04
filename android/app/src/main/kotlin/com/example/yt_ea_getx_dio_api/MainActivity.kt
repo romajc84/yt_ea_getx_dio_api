ea8a85fc15b02c4552eec8d7ffbcfb6119ba1ab5
romajc <joeyroman84@gmail.com>
2021-07-04 08:09:05 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/yt_ea_getx_dio_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yt_ea_getx_dio_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/service/http_service_impl.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:yt_ea_getx_dio_api/service/http_service.dart';
+
+class HttpServiceImpl implements HttpService {
+  @override
+  Future<Response> getRequest(String url) {
+    throw UnimplementedError();
+  }
+
+  @override
+  void init() {}
+}
